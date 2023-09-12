e0f087fcb397ce10abdc173f299cc237d4609f2d
SangMilyarder <nawawidarryl@gmail.com>
2023-09-12 15:20:11 +0700

    Tugas Django selesai

---
+++ main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    amount = models.IntegerField()
+    category = models.TextField()
+    description = models.TextField()
