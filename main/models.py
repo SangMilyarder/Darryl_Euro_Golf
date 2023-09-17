2bdc0b0605a0a9b6225ee98d65cb13f7e6fd71b4
SangMilyarder <nawawidarryl@gmail.com>
2023-09-17 17:27:13 +0700

    Update progress tugas 3

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # Create your models here.
 
-class Item(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     amount = models.IntegerField()
