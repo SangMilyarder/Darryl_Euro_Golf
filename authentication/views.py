12987ca001811596d69fb48a513c39928e6dffdd
SangMilyarder <nawawidarryl@gmail.com>
2023-11-22 10:49:37 +0700

    Fix CSRF error

--- authentication/views.py
+++ authentication/views.py
@@ -5,7 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.models import User
 
-@csrf_exempt
 def login(request):
     username = request.POST['username']
     password = request.POST['password']
@@ -33,7 +32,6 @@ def login(request):
             "message": "Login gagal, periksa kembali email atau kata sandi."
         }, status=401)
     
-@csrf_exempt
 def logout(request):
     username = request.user.username
 
@@ -50,7 +48,6 @@ def logout(request):
         "message": "Logout gagal."
         }, status=401)
     
-@csrf_exempt
 def register(request):
     if request.method == "POST":
         username = request.POST.get('username')
